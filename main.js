537205cbce6762bd720897437f1d930eb92527d7
Maulana Aziz <maulana.dijah45@gmail.com>
2024-12-22 23:23:05 +0700

    menambah file

---
+++ main.js
@@ -0,0 +1,8 @@
+function showMessage() {
+    const message = document.getElementById("message");
+    message.textContent = "Semoga semua harapanmu terkabul dan selalu diberikan kebahagiaan. Selamat Ulang Tahun!";
+    message.style.color = "#ff4500";
+    message.style.fontSize = "1.5em";
+    message.style.marginTop = "20px";
+}
+
